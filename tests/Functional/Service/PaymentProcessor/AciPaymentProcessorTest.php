6c53829cd9682c6a8f1bfe9c998fdb794a81af4b
Bosko Stupar <bosko.stupar@gmail.com>
2024-09-03 20:54:22 +0200

    Basic testing API calls to the ACI & Shift4 payment processors

---
+++ tests/Functional/Service/PaymentProcessor/AciPaymentProcessorTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Tests\Functional\Service\PaymentProcessor;
+
+use App\DTO\TransactionDTO;
+use App\Model\TransactionResponseModel;
+use App\Service\PaymentProcessor\AciPaymentProcessor;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class AciPaymentProcessorTest extends KernelTestCase
+{
+    public function testMakeTransactionReturnsCorrectResponse()
+    {
+        self::bootKernel();
+        $aciProcessorService = self::getContainer()->get(AciPaymentProcessor::class);
+        $date = (new \DateTime())->modify('+5 years');
+        $transactionDTO = new TransactionDTO(
+            cardNumber: '4200000000000000',
+            cardExpiryYear: $date->format('Y'),
+            cardExpiryMonth: $date->format('m'),
+            cardCvv: '123',
+            amount: '10',
+            currency: 'EUR'
+        );
+
+        $transaction = $aciProcessorService->makeTransaction($transactionDTO);
+
+        $this->assertInstanceOf(TransactionResponseModel::class, $transaction);
+        $this->assertEquals($transactionDTO->getCurrency(), $transaction->getTransactionCurrency());
+        $this->assertEquals(number_format($transactionDTO->getAmount(), 2), $transaction->getTransactionAmount());
+    }
+}
---
+++ tests/Functional/Service/PaymentProcessor/Shift4PaymentProcessorTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Tests\Functional\Service\PaymentProcessor;
+
+use App\DTO\TransactionDTO;
+use App\Model\TransactionResponseModel;
+use App\Service\PaymentProcessor\Shift4PaymentProcessor;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class Shift4PaymentProcessorTest extends KernelTestCase
+{
+    public function testMakeTransactionReturnsCorrectResponse()
+    {
+        self::bootKernel();
+        $shift4ProcessorService = self::getContainer()->get(Shift4PaymentProcessor::class);
+        $date = (new \DateTime())->modify('+5 years');
+        $transactionDTO = new TransactionDTO(
+            cardNumber: '4200000000000000',
+            cardExpiryYear: $date->format('Y'),
+            cardExpiryMonth: $date->format('m'),
+            cardCvv: '123',
+            amount: '10',
+            currency: 'EUR'
+        );
+
+        $transaction = $shift4ProcessorService->makeTransaction($transactionDTO);
+
+        $this->assertInstanceOf(TransactionResponseModel::class, $transaction);
+        $this->assertEquals($transactionDTO->getCurrency(), $transaction->getTransactionCurrency());
+        $this->assertEquals($transactionDTO->getAmount() * 100, $transaction->getTransactionAmount());
+    }
+}
